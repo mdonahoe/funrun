92cb43eb32042ed9a59bf64ddf1f6e444dcfedc6
Matt Donahoe <matt.donahoe@gmail.com>
2011-05-11 11:32:26 -0400

    TheCarMission created

---
+++ ontherun/Classes/TheCarMission.h
@@ -0,0 +1,39 @@
+//
+//  TheCarMission.h
+//  ontherun
+//
+//  Created by Matt Donahoe on 5/11/11.
+//  Copyright 2011 MIT Media Lab. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "FRMissionTemplate.h"
+
+@interface TheCarMission : FRMissionTemplate {
+    //states
+    int car_state;
+    int alarm_state;
+    int cop_state;
+    int safehouse_state;
+    int current_state;
+    
+    //audio
+    AVAudioPlayer * ulysses;
+    AVAudioPlayer * _music;
+    AVAudioPlayer * siren;
+    AVAudioPlayer * alarm;
+    
+    //points
+    FRPoint * car;
+    FRPoint * cop;
+    FRPoint * safehouse;
+}
+
+- (void) the_car;
+- (void) the_alarm;
+- (void) the_cop;
+- (void) the_safehouse;
+
+
+
+@end
---
+++ ontherun/Classes/TheCarMission.m
@@ -0,0 +1,14 @@
+//
+//  TheCarMission.m
+//  ontherun
+//
+//  Created by Matt Donahoe on 5/11/11.
+//  Copyright 2011 MIT Media Lab. All rights reserved.
+//
+
+#import "TheCarMission.h"
+
+
+@implementation TheCarMission
+
+@end
