ef5f6d9ea4214f69b225092916243316834e016e
Matt Donahoe <matt.donahoe@gmail.com>
2011-05-31 18:41:37 -0400

    direction bug

---
+++ ontherun/Classes/directionBug.h
@@ -0,0 +1,16 @@
+//
+//  directionBug.h
+//  ontherun
+//
+//  Created by Matt Donahoe on 5/20/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface directionBug : FRMissionTemplate {
+    
+}
+
+@end
---
+++ ontherun/Classes/directionBug.m
@@ -0,0 +1,14 @@
+//
+//  directionBug.m
+//  ontherun
+//
+//  Created by Matt Donahoe on 5/20/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "directionBug.h"
+
+
+@implementation directionBug
+
+@end
